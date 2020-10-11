b245ff9ad4360ef49b14d2216bda09687a5650d7
Nikita Baranov <khabnet47@mail.ru>
2020-10-11 14:20:41 +0300

    Updates

---
+++ precache-manifest.aafd0093563c827c75e81886aa195531.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f22b98e16d5973da5560f8588846523b",
+    "url": "/CRUD-announcement/index.html"
+  },
+  {
+    "revision": "c60207603fff99e31861",
+    "url": "/CRUD-announcement/static/css/main.b479b524.chunk.css"
+  },
+  {
+    "revision": "31ecff59a33b130eec48",
+    "url": "/CRUD-announcement/static/js/2.62e02ce2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CRUD-announcement/static/js/2.62e02ce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c60207603fff99e31861",
+    "url": "/CRUD-announcement/static/js/main.fedb1e57.chunk.js"
+  },
+  {
+    "revision": "abc95eb6320801e54458",
+    "url": "/CRUD-announcement/static/js/runtime-main.615ef13d.js"
+  },
+  {
+    "revision": "754f0c71969ab4ac7061100775d7dbe8",
+    "url": "/CRUD-announcement/static/media/sad-face.754f0c71.svg"
+  }
+]);
